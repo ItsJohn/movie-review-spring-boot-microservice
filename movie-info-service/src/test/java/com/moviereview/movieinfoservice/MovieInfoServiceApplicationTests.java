9d09a87b051428b308211cda70346f6450b824b4
John Kelly <john.j.kelly94@gmail.com>
2021-05-08 18:41:29 +0100

    Add the rest of the applications

---
+++ movie-info-service/src/test/java/com/moviereview/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviereview.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
