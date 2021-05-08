d17e37f3f3a6da8e7a3a9cb3611370891bcf12d5
John Kelly <john.j.kelly94@gmail.com>
2021-05-09 00:35:11 +0100

    created microservice calls

--- movie-catalog-service/src/main/java/com/moviereview/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/moviereview/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,21 +1,50 @@
 package com.moviereview.moviecatalogservice.resources;
 
 import com.moviereview.moviecatalogservice.models.CatalogItem;
+import com.moviereview.moviecatalogservice.models.Movie;
+import com.moviereview.moviecatalogservice.models.Rating;
+import com.moviereview.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    private <T>T callMircoService(String uri, Class<T> classType) {
+        return this.webClientBuilder.build()
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(classType)
+                .block();
+    }
+
     @GetMapping("/{userID}")
     public List<CatalogItem> getCatalog(@PathVariable("userID") String userID) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Robots turning into cars", 4)
-        );
+        UserRating userRating = callMircoService("http://localhost:8083/ratingsdata/users/" + userID, UserRating.class);
+
+        return userRating.getRatings().stream().map(rating -> {
+//            Rest Template is deprecated
+//            Movie movie = this.restTemplate.getForObject("http://localhost:8082/movies/3", Movie.class);
+
+            Movie movie = callMircoService("http://localhost:8082/movies/" + rating.getMovieID(), Movie.class);
+
+            return new CatalogItem(movie.getName(), "test", rating.getRating());
+        }).collect(Collectors.toList());
     }
 }
--- movie-data-service/src/main/java/com/moviereview/moviedataservice/resources/RatingResource.java
+++ movie-data-service/src/main/java/com/moviereview/moviedataservice/resources/RatingResource.java
@@ -1,16 +1,29 @@
 package com.moviereview.moviedataservice.resources;
 
 import com.moviereview.moviedataservice.models.Rating;
+import com.moviereview.moviedataservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResource {
-    @GetMapping("/movieID")
+    @GetMapping("/{movieID}")
     public Rating getRating(@PathVariable("movieID") String movieID) {
         return new Rating(movieID, 5);
+    }
+
+    @GetMapping("/users/{userID}")
+    public UserRating getUserRatings(@PathVariable("userID") String userID) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("3", 1),
+                new Rating("4", 4)
+        );
+        return new UserRating(ratings);
     }
 }
